38178fa2bb719778455628a8fabb608d9e7d1ba6
Billy Huang <billy@mitake.com.tw>
2023-06-06 15:53:08 +0800

    Initial Commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by RD1-Billy on 2023/6/6.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
